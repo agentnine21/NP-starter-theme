44da468ef1b93070ee07f3117b47aaf8a48affd4
agentnine21 <hello@agentnine21.com>
2015-04-03 14:18:02 -0400

    custom squeeze page template

--- page-full-width.php
+++ page-full-width.php
@@ -10,9 +10,22 @@
 get_header(); ?>
 
 <section id="primary" class="full-width" role="main">
-
-   <!-- Everything in here is the same as the default template shown above -->
-
+  
+      <?php while ( have_posts() ) : the_post(); ?>
+  
+          <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+              <header class="entry-header">
+                  <h1 class="entry-title"><?php the_title(); ?></h1>
+              </header><!-- .entry-header -->
+  
+              <div class="entry-content">
+                  <?php the_content(); ?>
+                  <?php wp_link_pages( array( 'before' => '<div class="page-link"><span>' . __( 'Pages:', 'themeTextDomain' ) . '</span>', 'after' => '</div>' ) ); ?>
+              </div><!-- .entry-content -->
+          </article><!-- #post-<?php the_ID(); ?> -->
+  
+      <?php endwhile; // end of the loop. ?>
+  
 </section><!-- #primary -->
 
 <?php get_footer(); ?>
---
+++ page-squeeze.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * Template Name: Squeeze Page
+ *
+ * This is a full width page template (no sidebar).
+ *
+ * @package A921_Starter_Theme
+ */
+
+get_header(); ?>
+
+<section id="primary" class="squeeze-page" role="main">
+
+    <?php while ( have_posts() ) : the_post(); ?>
+
+        <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+            <header class="entry-header">
+                <h1 class="entry-title"><?php the_title(); ?></h1>
+            </header><!-- .entry-header -->
+
+            <div class="entry-content">
+                <?php the_content(); ?>
+                <div>
+                	<a href="<?php the_field( 'button_url' ); ?>" class="button">
+                    		<?php the_field( 'button_text' ); ?>
+                	</a>
+                </div>
+                <?php wp_link_pages( array( 'before' => '<div class="page-link"><span>' . __( 'Pages:', 'themeTextDomain' ) . '</span>', 'after' => '</div>' ) ); ?>
+            </div><!-- .entry-content -->
+        </article><!-- #post-<?php the_ID(); ?> -->
+
+    <?php endwhile; // end of the loop. ?>
+
+</section><!-- #primary -->
+
+<?php get_footer(); ?>
